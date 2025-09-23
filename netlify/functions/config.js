fb7af231488ff0ce6eadb6a314ea34bd6f6c39ac
phuclee36ne <andersonparticia61@gmail.com>
2025-09-23 13:42:25 +0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '8323813502:AAGeMAk76aV2n2bxrNj5jxqNP2CfIqeD8f0',
-    chat_id: '-4936494750',
+    chat_id: '6864903293',
     noti_token: '',
     noti_chat_id: ''
 };
